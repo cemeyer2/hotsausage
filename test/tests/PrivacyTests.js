290d48a10f1caaee54b878a92ab82242c1353938
cemeyer2 <devnull@localhost>
2009-11-15 14:09:01 -0600

    work on privacy tests

--- test/tests/PrivacyTests.js
+++ test/tests/PrivacyTests.js
@@ -1,38 +1,59 @@
 describe('Priviledged test suite', function () { 
-	describe('When enabling private properties on an object', function () { 
-		var object = {};
-		var protectedProperties;
-		var key, count;
-		
-		it('should have one "private" function added', function () { 
-			count = 0;
-			for (key in object) {count += 1;};
-			expect(count).toBe(0); 
+	describe('When HotSausage.Privacy is loaded', function () { 
+		it('HotSausage.Privacy module should be accessible', function () {
 			
-			Priviledged.enableOn(obejct);
-			count = 0;
-			for (key in object) {count += 1;};
+			var priv = HotSausage.Privacy;
 			
-			expect(count).toBe(1);
-			expect(key).toBe("_pp");
-			expect(typeof obj._pp).toBe("function");
-			expect(obj._pp.length).toBe(1);
+			expect(priv).toBeDefined();
+			expect(priv.name()).toBe("Privacy");
+			expect(priv.module()).toBe(HotSausage);
+			expect(priv.newSubmodule).toBeDefined();
+			expect(priv.enableOn).toBeDefined();
+			expect(priv.lock).toBeDefined();
+			expect(priv.privilegedMethodOn).toBeDefined();
+			expect(priv.installCoreMethods).toBeDefined();
+			
+			priv.installCoreMethods();
+			
+			expect(Object.prototype.privilegedMethod).toBeDefined();
+			expect(Object.prototype.enablePrivacy).toBeDefined();
+			expect(Function.prototype.privilegedMethod).toBeDefined();
 		});
 		
-		it('should return an empty purse', function () { 
-			protectedProperties = Priviledged.enableOn(obejct);
-			count = 0;
-			for (key in object) {count += 1;};
-			expect(count).toBe(0);
+		it('should be able to enable privacy on an object', function () {
+			var Person = function(ssn){
+				var purse = this.enablePrivacy();
+				purse.ssn = ssn;
+				expect(purse).toBeDefined();
+				expect(purse.ssn).toBeDefined();
+				expect(purse.ssn).toEqual(ssn);
+			};
+			expect(Person.privilegedMethod).toBeDefined();
 			
-			expect(key).toBe("_pp");
-			expect(typeof obj._pp).toBe("function");
-			expect(obj._pp.length).toBe(1);
+			var p = new Person("123-45-6789");
 		});
 		
+		it('should be able to add a privileged method to an object', function () {
+			var pp;
+			
+			var Person = function (ssn) {
+				var purse = this.enablePrivacy();
+				purse.ssn = ssn;
+				pp = purse; //this is bad practice, but useful for testing
+			};
+			
+			Person.privilegedMethod("getSSN", function () {
+				expect(this).toEqual(pp);
+				expect(this.ssn).toBeDefined();
+				return this.ssn;
+			});
+			
+			var ssn = "123-45-6789";
+			var p = new Person(ssn);
+			
+			expect(p.getSSN).toBeDefined();
+			expect(p.getSSN()).toEqual(ssn);
 		
-		it('', function () {
-			// body...
-		})
+		});
 	});
 });
